0931c6450a2dc97b4df4959b459ef3ec8096a6e2
BalachandarG <balachandar.g@thoughtworks.com>
2024-04-25 12:22:05 +0530

    Swift library to decode and parse mdoc format VC

--- Example/mdocSpikeLibrary/ViewController.swift
+++ Example/mdocSpikeLibrary/ViewController.swift
@@ -7,18 +7,33 @@
 //
 
 import UIKit
+import mdocSpikeLibrary
+import SwiftCBOR
 
 class ViewController: UIViewController {
-
+    let library = CborLibrayUtils()
+    
+    @IBAction func parseMDocCBOR(_ sender: Any) {
+        
+        let parsedJson = library.decodeAndParseCBOR()
+        print("Parsed JSon From Library", parsedJson)
+    }
+    
+    @IBOutlet weak var parseMDocCBOR: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+   
 
 }
-
